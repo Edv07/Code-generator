db0d1a11b19038e20b2f33108a90c2ea469149a5
Edgar <edvmaster.com@gmail.com>
2022-12-11 23:53:48 -0600

    testing to see if commits work only added does button work console log

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log("Does button work");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
